4a98a3161a404680a946d206834faa66ac25e84d
wil <whenao86@gmail.com>
2022-03-20 13:38:04 -0500

    Crear test PUT success, PUT item no encontrado y GET success

--- src/test/java/com/example/RESTfulTest/controller/WidgetRestControllerTest.java
+++ src/test/java/com/example/RESTfulTest/controller/WidgetRestControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import java.util.Optional;
 
@@ -103,9 +104,75 @@ class WidgetRestControllerTest {
                 .andExpect(jsonPath("$.id", is(1)))
                 .andExpect(jsonPath("$.name", is("New Widget")))
                 .andExpect(jsonPath("$.description", is("This is my widget")))
+                .andExpect(jsonPath("$.version", is(1)))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("PUT /rest/widget/{id} success")
+    void testUpdateWidgetById() throws Exception {
+//        Setup mocked service
+        Widget widgetToUpdate = new Widget(1L, "New Widget", "This is my new widget", 1);;
+        Widget widgetToReturn = new Widget(1L, "Updated Widget", "This is my updated widget", 1);
+        doReturn(Optional.of(widgetToUpdate)).when(service).findById(1L);
+        doReturn(widgetToReturn).when(service).save(any());
+
+//        Execute the PUT request
+        mockMvc.perform(put("/rest/widget/{id}", 1L)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("If-Match", 1L)
+                .content(asJsonString(widgetToReturn)))
+//                Validate response code and content type
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//                Validate headers
+                .andExpect(header().string(HttpHeaders.LOCATION, "/rest/widget/1"))
+                .andExpect(header().string(HttpHeaders.ETAG, "\"1\""))
+//                Validate returned fields
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("Updated Widget")))
+                .andExpect(jsonPath("$.description", is("This is my updated widget")))
                 .andExpect(jsonPath("$.version", is(1)));
     }
 
+    @Test
+    @DisplayName("PUT /res/widget/{id} - not found")
+    void testUpdateWidgetByIdNotFound() throws Exception {
+        // Setup our mocked service
+        Widget widgetToReturn = new Widget(1L, "Updated Widget", "This is my updated widget", 1);
+        doReturn(Optional.empty()).when(service).findById(1l);
+
+        // Execute the PUT request
+        mockMvc.perform(put("/rest/widget/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header("If-Match", 1L)
+                        .content(asJsonString(widgetToReturn)))
+                // Validate the response code
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("GET /res/widget/{id} - success")
+    void testGetWidgetById() throws Exception {
+        Widget widgetToRetrieve = new Widget(1L, "Existing Widget", "This is an existing widget", 1);;
+        doReturn(Optional.of(widgetToRetrieve)).when(service).findById(1L);
+
+        //        Execute the PUT request
+        mockMvc.perform(get("/rest/widget/{id}", 1L))
+//                Validate response code and content type
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//                Validate headers
+                .andExpect(header().string(HttpHeaders.LOCATION, "/rest/widget/1"))
+//                Validate returned fields
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("Existing Widget")))
+                .andExpect(jsonPath("$.description", is("This is an existing widget")))
+                .andExpect(jsonPath("$.version", is(1)));
+
+    }
 
     static String asJsonString(final Object obj) {
         try {
